e57eae6ab539dcd2c768bec5e8c95d0c461eaaeb
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-27 02:29:38 +0500

    config, cors

--- internal/routes/config.go
+++ internal/routes/config.go
@@ -1,7 +1,10 @@
 package routes
 
 import (
+	"net/http"
 	"time"
+
+	"github.com/go-chi/cors"
 )
 
 type Config struct {
@@ -12,8 +15,22 @@ func NewConfig() *Config {
 	return &Config{}
 }
 
-func Cors()
+func (c *Config) Cors(next http.Handler) http.Handler {
+	return cors.New(cors.Options{
+		AllowedOrigins:     []string{"*"},
+		AllowedMethods:     []string{"*"},
+		AllowedHeaders:     []string{"*"},
+		ExposeHeaders:      []string{"*"},
+		AllowedCredentials: true,
+		MaxAge:             5,
+	}).Handler(next)
+}
 
-func SetTimeout()
+func (c *Config) SetTimeout(timeInSeconds int) *Config {
+	c.timeout = time.Duration(timeInSeconds) * time.Second
+	return c
+}
 
-func GetTimeout()
+func (c *Config) GetTimeout() time.Duration {
+	return c.timeout
+}
