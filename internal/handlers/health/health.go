84609f4b91f6e88e76639a9939925f140896f78c
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-31 05:41:22 +0500

    Handler

--- internal/handlers/health/health.go
+++ internal/handlers/health/health.go
@@ -28,8 +28,18 @@ func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
 	HttpStatus.StatusOK(w, r, "Service OK")
 }
 
-func Post()
+func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
+	HttpStatus.StatusMethodNotAllowed(w, r)
+}
 
-func Put()
+func (h *Handler) Put(w http.ResponseWriter, r *http.Request) {
+	HttpStatus.StatusMethodNotAllowed(w, r)
+}
 
-func Delete()
+func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
+	HttpStatus.StatusMethodNotAllowed(w, r)
+}
+
+func (h *Handler) Options(w http.ResponseWriter, r *http.Request) {
+	HttpStatus.StatusNoContent(w, r)
+}
