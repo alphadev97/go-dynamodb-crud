14e06f70a8f130859a14b8eab3ebc25fb42e383b
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-08-01 03:43:38 +0500

    ListAll

--- internal/controllers/product.go
+++ internal/controllers/product.go
@@ -2,7 +2,9 @@ package product
 
 import (
 	product "github.com/alphadev97/go-dynamodb-crud/internal/controllers"
+	"github.com/alphadev97/go-dynamodb-crud/internal/entities/product"
 	"github.com/alphadev97/go-dynamodb-crud/internal/repository/adapter"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	"github.com/google/uuid"
 )
 
@@ -35,7 +37,37 @@ func (c *Controller) ListOne(id uuid.UUID) (entity product.Product, err error) {
 
 }
 
-func (c *Controller) ListAll() (entities []product.Product, err error)
+func (c *Controller) ListAll() (entities []product.Product, err error) {
+	entities = []product.Product{}
+	var entity product.Product
+
+	filter := expression.Name("name").NotEqual(expression.Value(""))
+	condition, err := expression.NewBuilder().WithFilter(filter).Build()
+
+	if err != nil {
+		return entities, err
+	}
+
+	response, err := c.repository.FindAll(condition, entity.TableName())
+
+	if err != nil {
+		return entities, err
+	}
+
+
+	if response != nil {
+		for _, value : range response.Items {
+			entity, err := product.ParseDynamoAttributeToStruct()
+			if err != nil {
+				return entities, err
+			}
+
+			entities = append(entities, entity)
+		}
+	}
+
+	return entities, nil
+}
 
 func (c *Controller) Create(entity *product.Product) (uuid.UUID, error)
 
