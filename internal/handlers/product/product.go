9c3a6e22d1798330b050b326b61a0df0325b6f63
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-31 07:21:20 +0500

    getOne, getAll

--- internal/handlers/product/product.go
+++ internal/handlers/product/product.go
@@ -7,6 +7,8 @@ import (
 	"github.com/alphadev97/go-dynamodb-crud/internal/handlers"
 	"github.com/alphadev97/go-dynamodb-crud/internal/handlers/product"
 	"github.com/alphadev97/go-dynamodb-crud/internal/repository/adapter"
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
 )
 
 type Handler struct {
@@ -22,11 +24,40 @@ func NewHandler(repository adapter.Interface) handlers.Interface {
 	}
 }
 
-func Get()
+func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
+	if chi.URLParam(r, "ID") != "" {
+		h.getOne(w, r)
+	} else {
+		h.getAll(w, r)
+	}
+}
 
-func getOne()
+func (h *Handler) getOne(w http.ResponseWriter, r *http.Request) {
+	ID, err := uuid.Parse(chi.URLParam(r, "ID"))
 
-func getAll()
+	if err != nil {
+		HttpStatus.StatusBadRequest(w, r, errors.New("ID is not uuid valid"))
+		return
+	}
+
+	response, err := h.Controller.ListOne(ID)
+	if err != nil {
+		HttpStatus.StatusInternalServerError(w, r, err)
+		return
+	}
+
+	HttpStatus.StatusOK(w, r, response)
+}
+
+func (h *Handler) getAll(w http.ResponseWriter, r *http.Request) {
+	response, err := h.Controller.ListAll()
+	if err != nil {
+		HttpStatus.StatusInternalServerError(w, r, err)
+		return
+	}
+
+	HttpStatus.StatusOK(w, r, response)
+}
 
 func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 	productBody, err := h.getBodyAndValidate(r, uuid.Nil)
